2624acff1ec25ecab4c5d042d8dd41bb6fc767ec
lovebindal88 <lovebindal88@gmail.com>
2019-06-17 23:39:44 +0530

    with H clust

--- clustering techniques.R
+++ clustering techniques.R
@@ -11,5 +11,13 @@ library(data.table)
 pairs.panels(B,cex=1.5)
 plot(B$Fuel_Cost ~ B$Sales)
 #adding labels to plot
-with(A,text(Fuel_Cost~Sales, labels= A$utility_name, pos= 4,cex= 0.5))
-
+with(A,text(Fuel_Cost~Sales, labels= A$utility_name, pos= 4,cex= 0.7))
+#use one hot encoding
+B= one_hot(as.data.table(A))
+scaledata= as.matrix(scale(B));scaledata
+#using kmeans
+m1 = kmeans(scaledata,3)
+plot(B$Fuel_Cost~ B$Sales, col = m1$cluster)
+#using hclust
+modelh= hclust(dist(scaledata), method = "complete") 
+plot(modelh, labels= A$utility_name)
