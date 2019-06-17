166c5ed7536d7de71055a8ef29f1b180b5a82c1b
lovebindal88 <lovebindal88@gmail.com>
2019-06-17 21:41:22 +0530

    plot with text and with

--- clustering techniques.R
+++ clustering techniques.R
@@ -1,8 +1,14 @@
 # new clustering 
 A = read.csv(file.choose())
 str(A)
+B= A[,-2]
+str(B)
 library(psych)
 library(caret)
 library(class)
 library(mltools)
 library(data.table)
+pairs.panels(B,cex=1.5)
+plot(B$Fuel_Cost ~ B$Sales, text = B$utility_name)
+with(A,text(Fuel_Cost~Sales, labels= A$utility_name))
+
